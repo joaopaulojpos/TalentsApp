cd7ff34ec09b99eba7d1e6b86b56e49608b5d866
TJ Borges <tj.batera1@gmail.com>
2018-02-18 11:08:51 -0300

    Alteração das Pags e começo da pag Perfil

--- Talents/src/app/app.module.ts
+++ Talents/src/app/app.module.ts
@@ -3,34 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage   } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage    } from '../pages/home/home';
 import { TabsPage    } from '../pages/tabs/tabs';
 import { LoginPage   } from '../pages/login/login';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { VagasPageModule } from '../pages/vagas/vagas.module';
+import { FavoritasPageModule } from '../pages/favoritas/favoritas.module';
+import { PerfilPageModule } from '../pages/perfil/perfil.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    VagasPageModule,
+    FavoritasPageModule,
+    PerfilPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     LoginPage
   ],
--- Talents/src/pages/tabs/tabs.ts
+++ Talents/src/pages/tabs/tabs.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { VagasPage } from '../vagas/vagas';
+import { FavoritasPage } from '../favoritas/favoritas';
+import { PerfilPage } from '../perfil/perfil';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = VagasPage;
+  tab2Root = FavoritasPage;
+  tab3Root = PerfilPage
+    ;
 
   constructor() {
 
