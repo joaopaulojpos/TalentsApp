26a6e58a1bd0fdbeb21a0b014b5996e7b98bf141
TJ Borges <tj.batera1@gmail.com>
2018-06-05 00:03:07 -0300

    Corrigindo erro de Layout no Login

--- Talents/src/providers/servicos/servicos.ts
+++ Talents/src/providers/servicos/servicos.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map';
 export class ServicosProvider {
   
   //public endereco = "http://talents.heliohost.org/api/public/api/";
-  //public endereco = "http://localhost/talentsweb/api/public/api/";
+  //public endereco = "http://192.135.13.71/talentsweb/api/public/api/";
   public endereco = "http://plataformatalent.tmp.k8.com.br/api/public/api/";
   
   constructor(public http: Http) 
